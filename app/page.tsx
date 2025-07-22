b413608289f1a596b4256cbfa5fed158a53903bb
Mostafa <candlelearner.m.dev@gmail.com>
2025-07-22 18:16:40 +0100

    working on mobile navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import AboutPreview from "@/components/AboutPreview";
 import ContactPreview from "@/components/ContactPreview";
 import Hero from "@/components/Hero";
-import MobileHero from "@/components/Hero/MobileHero";
 import PartnersPreview from "@/components/PartnersPreview";
 import ServicesPreview from "@/components/ServicesPreview/ServicesPreview";
 import TestimonialsSection from "@/components/TestimonialsSection";
@@ -9,7 +8,7 @@ import TestimonialsSection from "@/components/TestimonialsSection";
 export default function Home() {
   return (
     <main>
-      <MobileHero />
+      <Hero />
       <AboutPreview />
       <ServicesPreview />
       <PartnersPreview />
